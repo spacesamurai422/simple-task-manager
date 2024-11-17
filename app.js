cda9f7175419d07180caf3261297a541e721bfc8
spacesamurai422 <spacesamurai422@gmail.com>
2024-11-17 15:07:45 +0900

    Added missing express json parse in app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
+// Middleware to parse JSON req body
+app.use(express.json());
+
 // Error handling with middleware yet to be added
 
 app.use('/tasks', taskRoutes);
